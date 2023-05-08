c007d0dad3fa185c9328e0b97d139b00076d28fa
adnfalll___ <118400004+adnanzainal21@users.noreply.github.com>
2023-05-08 22:11:57 +0700

    Create varib.java
    variabel dan konstata pada java

---
+++ Java/varib.java
@@ -0,0 +1,9 @@
+int angka = 10; // variabel angka bertipe data integer dengan nilai awal 10
+double harga = 5000.0; // variabel harga bertipe data double dengan nilai awal 5000.0
+String nama = "adnan zainal"; // variabel nama bertipe data string dengan nilai awal "John Doe"
+boolean status = true; // variabel status bertipe data boolean dengan nilai awal true
+
+
+final double PI = 3.14; // konstanta PI dengan nilai 3.14
+final String WEBSITE_URL = "https://www.example.com"; // konstanta WEBSITE_URL dengan nilai "https://www.example.com"
+final int MAX_VALUE = 100; // konstanta MAX_VALUE dengan nilai 100
